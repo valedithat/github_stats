bf60039bf67b3c1e0e1a198799625d5c9de571bb
valedithat <valedithat@pm.me>
2022-03-06 23:12:53 -0500

    add rest_framework, update installed apps

--- github_stats/settings.py
+++ github_stats/settings.py
@@ -31,16 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'github_profiles.apps.GithubProfilesConfig',
-    'goals.apps.GoalsConfig',
-    'peers.apps.PeersConfig',
-    'users.apps.UsersConfig',
+    'github_profiles',
+    'goals',
+    'peers',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
